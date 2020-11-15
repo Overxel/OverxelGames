c5fc67c3f907a7f20a9b26c1c842133d1786c746
DESKTOP-CHTCJ5U\galai <galaico210@gmail.com>
2020-11-15 21:02:41 +0100

    WIP UI UX Navbar

--- src/app/components/shared/nav/nav.component.ts
+++ src/app/components/shared/nav/nav.component.ts
@@ -1,15 +1,17 @@
+import { NavService } from './../../../services/nav.service';
 import { Component, OnInit } from '@angular/core';
-
+import { NavLink } from 'src/app/interfaces/interfaces';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
+  public navLinks: NavLink[];
 
-  constructor() { }
+  constructor(private navService: NavService) {}
 
   ngOnInit(): void {
+    this.navLinks = this.navService.getNavLinks();
   }
-
 }
--- src/app/services/igdb.service.ts
+++ src/app/services/igdb.service.ts
@@ -2,23 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class IgdbService {
   queryLimit = 50;
   httpHeaders = {
     'Client-ID': environment.IGDB.ClientID,
-    'Authorization': environment.IGDB.Authorization
+    Authorization: environment.IGDB.Authorization,
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getSearch(search: string) {
-    let query = "fields *; search " + search + "; limit " + this.queryLimit;
-    return this.http.post(environment.IGDB.URL + "search", 
-                          query, 
-                          {
-                            headers :new HttpHeaders(this.httpHeaders)
-                          });
+    const query = 'fields *; search ' + search + '; limit ' + this.queryLimit;
+    return this.http.post(environment.IGDB.URL + 'search', query, {
+      headers: new HttpHeaders(this.httpHeaders),
+    });
   }
 }
